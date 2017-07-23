9af89b9d1cae857c3f008cd4645b6ff860793895
jasoncheng7115 <sanyu3u@gmail.com>
2017-07-23 12:20:02 +0800

    第一個版本

---
+++ PDF轉向小工具/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("微軟正黑體", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Padding = New System.Windows.Forms.Padding(5)
+        Me.Button1.Size = New System.Drawing.Size(131, 49)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "向右轉90度"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("微軟正黑體", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(12, 122)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Padding = New System.Windows.Forms.Padding(5)
+        Me.Button2.Size = New System.Drawing.Size(131, 49)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "旋轉180度"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("微軟正黑體", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button3.Location = New System.Drawing.Point(12, 67)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Padding = New System.Windows.Forms.Padding(5)
+        Me.Button3.Size = New System.Drawing.Size(131, 49)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "向左轉90度"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 182)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(131, 20)
+        Me.LinkLabel1.TabIndex = 4
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        Me.LinkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(155, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(473, 188)
+        Me.TextBox1.TabIndex = 5
+        Me.TextBox1.Text = "請將欲轉向之 PDF 檔案拖曳至此視窗"
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 214)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "PDF 轉向小工具 by Jason Cheng"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ PDF轉向小工具/Form1.vb
@@ -0,0 +1,162 @@
+﻿Imports iTextSharp.text.pdf
+Imports iTextSharp.text.pdf.parser
+
+Public Class Form1
+
+    Dim DragDropFileName As String = ""
+    Dim NewTargetPdfFileName As String = ""
+    Dim SelectFileName As String = ""
+    Dim SelectFileNameText As String = ""
+
+    ''' <summary>
+    ''' 處理檔案拖曳行為
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+
+        If files.Count > 1 Then
+
+            '超過數量，不予處理
+            LinkLabel1.Visible = False
+            MsgBox("一次只能轉換一個檔案", MsgBoxStyle.Exclamation, "提醒")
+
+        Else
+
+            '記錄與顯示相關檔名資訊
+            LinkLabel1.Visible = False
+            DragDropFileName = files(0).ToString()
+            SelectFileName = files(0).ToString()
+            SelectFileNameText = "您已選擇 '" & files(0).ToString() & "' 檔案進行轉向"
+            TextBox1.Text = SelectFileNameText
+
+        End If
+
+
+    End Sub
+
+    ''' <summary>
+    ''' 起用拖曳進入行為
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+
+        If e.Data.GetDataPresent(DataFormats.FileDrop, False) Then
+            e.Effect = DragDropEffects.All
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 表單載入
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        LinkLabel1.Visible = False
+        LinkLabel1.Text = "[開啟檔案]"
+
+    End Sub
+
+    ''' <summary>
+    ''' 進行轉向處理
+    ''' </summary>
+    ''' <param name="SourcePdfFileName">來源檔名與路徑</param>
+    ''' <param name="rotation">轉向角度</param>
+    ''' <remarks></remarks>
+    Sub DoRotate(SourcePdfFileName As String, rotation As Integer)
+
+        '沒有檔案
+        If DragDropFileName = "" Then
+            MsgBox("您尚未拖曳要轉向之檔案進來", MsgBoxStyle.Exclamation, "提醒")
+            Exit Sub
+        End If
+
+        Try
+
+            '組成新檔名
+            NewTargetPdfFileName = ""
+            NewTargetPdfFileName &= IO.Path.GetDirectoryName(SourcePdfFileName) & "\" & IO.Path.GetFileNameWithoutExtension(SourcePdfFileName) & "_" & rotation.ToString & IO.Path.GetExtension(SourcePdfFileName)
+
+            '讀入來源檔
+            Dim reader As New PdfReader(SourcePdfFileName)
+            Dim pagesCount As Integer = reader.NumberOfPages()
+
+            For n As Integer = 1 To pagesCount
+
+                TextBox1.Text = SelectFileNameText & vbCrLf & vbCrLf & "轉換進度： " & n & "/" & pagesCount & " 頁"
+
+                Dim page As PdfDictionary = reader.GetPageN(n)
+
+                '轉向並回寫至頁面
+                Dim rotate As PdfNumber = page.GetAsNumber(PdfName.ROTATE)
+                page.Put(PdfName.ROTATE, New PdfNumber(rotation))
+
+                '釋放一下 CPU
+                Threading.Thread.Sleep(1)
+
+            Next
+
+            '寫入新檔
+            Dim Stamper As PdfStamper = New PdfStamper(reader, New IO.FileStream(NewTargetPdfFileName, IO.FileMode.Create))
+
+            Stamper.Close()
+            reader.Close()
+
+            TextBox1.Text = SelectFileNameText & vbCrLf & vbCrLf & "轉換完成，檔案 '" & NewTargetPdfFileName & "'"
+            LinkLabel1.Visible = True
+
+        Catch ex As Exception
+
+            '顯示錯誤結果
+            MsgBox("寫入 '" & NewTargetPdfFileName & "' 時發生錯誤：" & ex.Message, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "錯誤")
+            TextBox1.Text = SelectFileNameText & vbCrLf & vbCrLf & "轉換發生問題，請注意目的地是否無權限或被開啟中，或是非 PDF 格式。"
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        CType(sender, Button).Enabled = False
+        DoRotate(DragDropFileName, 90)
+        CType(sender, Button).Enabled = True
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        CType(sender, Button).Enabled = False
+        DoRotate(DragDropFileName, 270)
+        CType(sender, Button).Enabled = True
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        CType(sender, Button).Enabled = False
+        DoRotate(DragDropFileName, 180)
+        CType(sender, Button).Enabled = True
+
+    End Sub
+
+    ''' <summary>
+    ''' 開啟轉換完成之檔案
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        LinkLabel1.LinkVisited = True
+        System.Diagnostics.Process.Start(NewTargetPdfFileName)
+    End Sub
+
+End Class
